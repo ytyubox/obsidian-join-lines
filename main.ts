af4f7950f2bc07c7d97efa4b152478306270262e
Tsungyu Yu <ytyubox@gmail.com>
2024-11-21 11:15:43 +0800

    TCR Thu Nov 21 11:15:43 CST 2024

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ function joinLinesSelectText(text: string) {
 	return text.replace(/\n/g, " ");
 }
 
-function joinLinesCursorText(text: string) {}
+function joinLinesCursorText(curren: string) {}
 function joinLines(editor: Editor) {
 	const selectedText = editor.getSelection();
 	if (selectedText && selectedText.includes("\n")) {
