dcb6228f56679adef756b7592403df45c121baed
Tsungyu Yu <ytyubox@gmail.com>
2024-11-21 14:16:41 +0800

    TCR Thu Nov 21 14:16:41 CST 2024

--- main.ts
+++ main.ts
@@ -19,7 +19,34 @@ export default class JoinLinesPlugin extends Plugin {
 	}
 }
 
-function joinLines(editor: Editor) {
+function joinNextLines(editor: Editor) {
+	const selectedText = editor.getSelection();
+	if (selectedText && selectedText.includes("\n")) {
+		const joinedText = joinLinesSelectText(selectedText);
+		editor.replaceSelection(joinedText);
+		return;
+	}
+	// No text selected:
+	// join current line with next line,
+	// and preserve the cursor position
+	const cursor = editor.getCursor();
+	const currLine = cursor.line;
+	const nextLine = currLine + 1;
+	const currLineText = editor.getLine(currLine);
+	const nextLineText = editor.getLine(nextLine);
+	const [joinedText, cursorCH] = joinLinesCursorText(
+		currLineText,
+		nextLineText
+	);
+	editor.replaceRange(
+		joinedText,
+		{ line: currLine, ch: 0 },
+		{ line: nextLine, ch: nextLineText.length }
+	);
+	editor.setCursor({ line: currLine, ch: cursorCH });
+}
+
+function joinPreviousLines(editor: Editor) {
 	const selectedText = editor.getSelection();
 	if (selectedText && selectedText.includes("\n")) {
 		const joinedText = joinLinesSelectText(selectedText);
