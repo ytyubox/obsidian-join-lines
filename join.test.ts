a94a3fde8edb9421d7d0d1e6bb64e31893714537
Tsungyu Yu <ytyubox@gmail.com>
2024-11-21 11:46:52 +0800

    TCR Thu Nov 21 11:46:52 CST 2024

--- join.test.ts
+++ join.test.ts
@@ -52,45 +52,4 @@ describe("join cursor line", () => {
 	it("empty select", () => {
 		expect(joinLinesSelectText("")).toStrictEqual("");
 	});
-
-	it("1 line selection", () => {
-		expect(joinLinesSelectText("line 1")).toStrictEqual("line 1");
-	});
-
-	it("2 lines selection", () => {
-		expect(
-			joinLinesSelectText(dedent`
-				line 1
-				line 2
-				`)
-		).toStrictEqual("line 1 line 2");
-	});
-
-	it("too many linebreak with 2 lines selection", () => {
-		expect(
-			joinLinesSelectText(dedent`
-				line 1
-
-
-
-				line 2
-				`)
-		).toStrictEqual("line 1 line 2");
-	});
-	it("too many linebreak with 3 lines selection", () => {
-		expect(
-			joinLinesSelectText(dedent`
-				line 1
-
-
-				
-				line 2
-
-
-
-				
-				line 3
-				`)
-		).toStrictEqual("line 1 line 2 line 3");
-	});
 });
