ae87a0819f32656ff40768dccb7da87729407f56
Tsungyu Yu <ytyubox@gmail.com>
2024-11-21 11:46:31 +0800

    TCR Thu Nov 21 11:46:31 CST 2024

--- join.test.ts
+++ join.test.ts
@@ -47,3 +47,50 @@ describe("join selected lines", () => {
 		).toStrictEqual("line 1 line 2 line 3");
 	});
 });
+
+describe("join selected lines", () => {
+	it("empty select", () => {
+		expect(joinLinesSelectText("")).toStrictEqual("");
+	});
+
+	it("1 line selection", () => {
+		expect(joinLinesSelectText("line 1")).toStrictEqual("line 1");
+	});
+
+	it("2 lines selection", () => {
+		expect(
+			joinLinesSelectText(dedent`
+				line 1
+				line 2
+				`)
+		).toStrictEqual("line 1 line 2");
+	});
+
+	it("too many linebreak with 2 lines selection", () => {
+		expect(
+			joinLinesSelectText(dedent`
+				line 1
+
+
+
+				line 2
+				`)
+		).toStrictEqual("line 1 line 2");
+	});
+	it("too many linebreak with 3 lines selection", () => {
+		expect(
+			joinLinesSelectText(dedent`
+				line 1
+
+
+				
+				line 2
+
+
+
+				
+				line 3
+				`)
+		).toStrictEqual("line 1 line 2 line 3");
+	});
+});
