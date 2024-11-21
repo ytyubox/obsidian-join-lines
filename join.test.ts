49cff731768f62a67fea6a698c52c1098404ea3d
Tsungyu Yu <ytyubox@gmail.com>
2024-11-21 11:45:35 +0800

    TCR Thu Nov 21 11:45:35 CST 2024

--- join.test.ts
+++ join.test.ts
@@ -30,7 +30,7 @@ describe("join selected lines", () => {
 				`)
 		).toStrictEqual("line 1 line 2");
 	});
-	it("too many linebreak with 2 lines selection", () => {
+	it("too many linebreak with 3 lines selection", () => {
 		expect(
 			joinLinesSelectText(dedent`
 				line 1
