d12175d3a20beee422bc9dac1093c643d29b0d5b
Tsungyu Yu <ytyubox@gmail.com>
2024-11-21 11:14:00 +0800

    TCR Thu Nov 21 11:14:00 CST 2024

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ export default class JoinLinesPlugin extends Plugin {
 	}
 }
 
-function joinLinesSelect
+function joinLinesSelectText(str)
 
 function joinLines(editor: Editor) {
 	const selectedText = editor.getSelection();
