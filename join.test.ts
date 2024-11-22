1d992400543b35c5602a26b4c79b33f69a2a6e4d
Tsungyu Yu <ytyubox@gmail.com>
2024-11-22 14:58:21 +0800

    TCR Fri Nov 22 14:58:21 CST 2024

--- join.test.ts
+++ join.test.ts
@@ -107,7 +107,12 @@ describe("test join behavior", () => {
 					joinNextLine("1. current line", "# next line")
 				).toStrictEqual(["1. current line next line", 16]);
 			});
-			it("line, next line not heading, prefixxed", () => {
+			it("line, next line heading, space suffix", () => {
+				expect(
+					joinNextLine("1. current line", "# next line   ")
+				).toStrictEqual(["1. current line next line", 16]);
+			});
+			it("line, next line heading ", () => {
 				expect(
 					joinNextLine("1. current line", "  # next line")
 				).toStrictEqual(["1. current line # next line", 16]);
