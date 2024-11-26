1be48afe92817cde2014112cc54775c1217f7802
Tsungyu Yu <ytyubox@gmail.com>
2024-11-26 18:22:59 +0800

    TCR Tue Nov 26 18:22:59 CST 2024

--- join.ts
+++ join.ts
@@ -76,48 +76,48 @@ function trimMarkdownListSymbol(lineText: string): string {
 	return lineText.replace(/^\s*([-*+]\s+|\d+\.\s+)/, "").trim();
 }
 
-function joinLinesCursorTextWithLists(
-	currLineText: string,
-	nextLineText: string
-): [string, number] {
-	currLineText = currLineText.trimEnd();
-	if (isNullOrEmpty(nextLineText)) {
-		return [currLineText, currLineText.length];
-	}
-	const [currLineLevel, currRestLine] = checkIndentLevel(currLineText);
-	const [nextLineLevel, nextRestLine] = checkIndentLevel(nextLineText);
-	if (currLineLevel >= nextLineLevel) {
-		// Remove numbering (e.g., "2. ") from the next line
-		nextLineText = trimMarkdownListSymbol(nextRestLine);
+// function joinLinesCursorTextWithLists(
+// 	currLineText: string,
+// 	nextLineText: string
+// ): [string, number] {
+// 	currLineText = currLineText.trimEnd();
+// 	if (isNullOrEmpty(nextLineText)) {
+// 		return [currLineText, currLineText.length];
+// 	}
+// 	const [currLineLevel, currRestLine] = checkIndentLevel(currLineText);
+// 	const [nextLineLevel, nextRestLine] = checkIndentLevel(nextLineText);
+// 	if (currLineLevel >= nextLineLevel) {
+// 		// Remove numbering (e.g., "2. ") from the next line
+// 		nextLineText = trimMarkdownListSymbol(nextRestLine);
 
-		return [currLineText + " " + nextLineText, currLineText.length + 1];
-	}
+// 		return [currLineText + " " + nextLineText, currLineText.length + 1];
+// 	}
 
-	const currLineorder = currLineText.match(/^\s*(\d+)\.\s*/g);
-	const nextLineorder = nextLineText.match(/^\s*(\d+)\.\s*/);
+// 	const currLineorder = currLineText.match(/^\s*(\d+)\.\s*/g);
+// 	const nextLineorder = nextLineText.match(/^\s*(\d+)\.\s*/);
 
-	if (currLineorder && nextLineorder) {
-		const nextNumber = parseInt(currLineorder[0]) + 1;
-		nextLineText = nextLineText
-			.replace(/^\s*\d+\.\s*/, `${nextNumber}. `)
-			.trim();
-		if (currLineLevel > 0) {
-			nextLineText = " ".repeat(currLineLevel) + nextLineText;
-		}
-		const line = `${currLineText}\n${nextLineText}`;
-		return [line, currLineText.length];
-	}
-	const currLinelistMark = currLineText.match(/\s*(.*)\s/);
-	if (currLinelistMark) {
-		const listMark = currLinelistMark[0];
-		nextLineText = nextLineText
-			.replace(/^\s*[-*+]\.\s*/, `${listMark} `)
-			.trim();
-		if (currLineLevel > 0) {
-			nextLineText = " ".repeat(currLineLevel) + nextLineText;
-		}
-		const line = `${currLineText}\n${nextLineText}`;
-		return [line, 0];
-	}
-	return ["__FAILURE_CASE__", -9999999999999];
-}
+// 	if (currLineorder && nextLineorder) {
+// 		const nextNumber = parseInt(currLineorder[0]) + 1;
+// 		nextLineText = nextLineText
+// 			.replace(/^\s*\d+\.\s*/, `${nextNumber}. `)
+// 			.trim();
+// 		if (currLineLevel > 0) {
+// 			nextLineText = " ".repeat(currLineLevel) + nextLineText;
+// 		}
+// 		const line = `${currLineText}\n${nextLineText}`;
+// 		return [line, currLineText.length];
+// 	}
+// 	const currLinelistMark = currLineText.match(/\s*(.*)\s/);
+// 	if (currLinelistMark) {
+// 		const listMark = currLinelistMark[0];
+// 		nextLineText = nextLineText
+// 			.replace(/^\s*[-*+]\.\s*/, `${listMark} `)
+// 			.trim();
+// 		if (currLineLevel > 0) {
+// 			nextLineText = " ".repeat(currLineLevel) + nextLineText;
+// 		}
+// 		const line = `${currLineText}\n${nextLineText}`;
+// 		return [line, 0];
+// 	}
+// 	return ["__FAILURE_CASE__", -9999999999999];
+// }
